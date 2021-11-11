7c6c7c96759e5a92f11693a843f0dccf5fa126bb
Luis Carlos <luisca_jl@hotmail.com>
2021-11-11 15:39:18 +0100

    new torrentYear

--- src/main/java/es/lavanda/lib/common/model/FilmModelTorrent.java
+++ src/main/java/es/lavanda/lib/common/model/FilmModelTorrent.java
@@ -1,8 +1,10 @@
 package es.lavanda.lib.common.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = true, exclude = "torrentYear")
 public class FilmModelTorrent extends TorrentModel {
 
     private int torrentYear;
