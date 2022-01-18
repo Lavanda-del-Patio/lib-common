27d9c9a9c4ba35b37b6e9f3dfec8cc6fdb0c5940
Luis Carlos <luisca_jl@hotmail.com>
2022-01-18 19:34:02 +0100

    Update ODTO

--- src/main/java/es/lavanda/lib/common/model/FilebotExecutionODTO.java
+++ src/main/java/es/lavanda/lib/common/model/FilebotExecutionODTO.java
@@ -1,8 +1,16 @@
 package es.lavanda.lib.common.model;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 @Data
-public class FilebotExecutionODTO {
-    
+public class FilebotExecutionODTO implements Serializable{
+
+    private String id;
+
+    private boolean forceQuery;
+
+    private String query;
+
 }
