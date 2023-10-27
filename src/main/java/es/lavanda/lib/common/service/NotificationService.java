271a24e1bd621224eb05fe9d06243d3730b19e0d
Luis Carlos <luisca_jl@hotmail.com>
2023-10-27 20:16:09 +0200

    Message

--- src/main/java/es/lavanda/lib/common/service/NotificationService.java
+++ src/main/java/es/lavanda/lib/common/service/NotificationService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
 import org.springframework.stereotype.Service;
 
-import es.lavanda.lib.common.SnsTopic;
 
 // @Service
 public class NotificationService {
@@ -12,8 +11,8 @@ public class NotificationService {
     // @Autowired
     // private NotificationMessagingTemplate messagingTemplate;
 
-    public void send(SnsTopic topic, String message, String subject) {
+    // public void send(SnsTopic topic, String message, String subject) {
         // messagingTemplate.sendNotification(topic.getValue(), message, subject);
-    }
+    // }
 
 }
